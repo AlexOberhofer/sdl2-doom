98394bdf8c0ab711b79214ed544ce35c158b59d7
Alex Oberhofer <alexmoberhofer@gmail.com>
2021-12-22 18:05:48 -0500

    don't run executable after build

--- build_nix.sh
+++ build_nix.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-cd src && make clean && make -j4 && cp ./sdl2-doom ../sdl2-doom && ./sdl2-doom
+cd src && make clean && make -j4 && cp ./sdl2-doom ../sdl2-doom
--- build_windows.sh
+++ build_windows.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-cd src && make clean && make -j4 -f makefile.mingw && cp ./sdl2-doom.exe ../sdl2-doom.exe && ./sdl2-doom.exe
+cd src && make clean && make -j4 -f makefile.mingw && cp ./sdl2-doom.exe ../sdl2-doom.exe
