4abbcf018273d9ad2998e433657434f0d7ba510a
Alex Oberhofer <alexmoberhofer@gmail.com>
2021-12-26 19:16:36 -0500

    fixes for mingw build and header setup

--- mingw-sdl-setup.sh
+++ mingw-sdl-setup.sh
@@ -14,6 +14,17 @@ tar -xvzf SDL2-devel-2.0.18-mingw.tar.gz
 
 rm SDL2-devel-2.0.18-mingw.tar.gz
 
+wget https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-devel-2.0.4-mingw.tar.gz
+
+tar -xvzf SDL2_mixer-devel-2.0.4-mingw.tar.gz
+
+#voodoo and this needs fixed but now required for cross compile build
+cp ./SDL2_mixer-2.0.4/x86_64-w64-mingw32/include/SDL2/SDL_mixer.h ./SDL2-2.0.18/x86_64-w64-mingw32/include/SDL2/
+
+cp ./SDL2_mixer-2.0.4/x86_64-w64-mingw32/bin/SDL2_mixer.dll ./
+
+rm SDL2_mixer-devel-2.0.4-mingw.tar.gz
+
 #SDL dll
 wget https://www.libsdl.org/release/SDL2-2.0.18-win32-x64.zip
 
